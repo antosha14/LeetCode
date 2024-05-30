f64531bd26ff75f381dbc19a4077c7d68d410318
unknown <anton.kozel.97@mail.ru>
2024-05-30 09:13:27 +0300

    learning: 30.05

--- Leetcode/Javascript/JS_Theory/7. Async.js
+++ Leetcode/Javascript/JS_Theory/7. Async.js
@@ -115,3 +115,31 @@ Promise.all([
 ]).then(alert); // когда все промисы выполнятся, результат будет 1,2,3
 // каждый промис даёт элемент массива
 `
+
+`
+Промиссификация (обёртка функции в промис)
+// promisify(f, true), чтобы получить массив результатов
+function promisify(f, manyArgs = false) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function callback(err, ...results) { // наш специальный колбэк для f
+        if (err) {
+          reject(err);
+        } else {
+          // делаем resolve для всех results колбэка, если задано manyArgs
+          resolve(manyArgs ? results : results[0]);
+        }
+      }
+
+      args.push(callback);
+
+      f.call(this, ...args);
+    });
+  };
+};
+
+// использование:
+f = promisify(f, true);
+f(...).then(arrayOfResults => ..., err => ...)
+Также существуют модули с более гибкой промисификацией, например, es6-promisify или встроенная функция util.promisify в Node.js.
+`
